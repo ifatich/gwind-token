6c351110b7a83fc77392f4f3beee9228f932f2b4
Lord Milano <lm@Lords-MacBook-Pro.local>
2025-06-20 16:35:28 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export {default as gwindTheme} from './libraries'
 export * from './libraries'
 export * from './plugins'
+
+console.info('%cGwind UI by Fatich Imam Al Arasy', 'color: #22c55e; font-weight: bold; font-size: 16px;');
